6755597a1d37b8f7924d1cb3344e547acddf2d9a
Christophe Tafani-Dereeper <christophetd@hotmail.fr>
2018-03-01 09:03:23 +0100

    Avoid using the deprecated 'assertEquals' method

--- test/test_saved_credentials_extraction.py
+++ test/test_saved_credentials_extraction.py
@@ -9,7 +9,7 @@ class TestSavedCredentialsExtraction(unittest.TestCase):
         profile_path = os.path.join(current_dir, "resources", "unencrypted-profile")
         credentials = get_saved_credentials(profile_path, master_password=None)
         self.assertEqual(len(credentials), 1)
-        self.assertEquals(credentials[0], {
+        self.assertEqual(credentials[0], {
             'url': 'https://github.com',
             'username': 'christophetd',
             'password': 'mypassword'
@@ -20,7 +20,7 @@ class TestSavedCredentialsExtraction(unittest.TestCase):
         profile_path = os.path.join(current_dir, "resources", "encrypted-profile")
         credentials = get_saved_credentials(profile_path, master_password="nevergonnagiveyouup")
         self.assertEqual(len(credentials), 1)
-        self.assertEquals(credentials[0], {
+        self.assertEqual(credentials[0], {
             'url': 'https://github.com',
             'username': 'christophetd',
             'password': 'mypassword'
